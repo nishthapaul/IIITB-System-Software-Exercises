00cca68b0805c98db795b58d75a472bca1aeee6a
Nishtha Paul <nishthapaul@Nishthas-MacBook-Air.local>
2023-08-04 10:29:59 +0530

    Add | menu boiler plate

---
+++ employee-record-program/employee-record.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "Please choose from the folowing menu to operate on employee records"
+select option in create delete search sort display_all exit
+do
+	echo option selected is ::: $option
+	case $option in
+		create )
+			echo create selected ;;
+		delete )
+			echo delete selected ;;
+		search )
+			echo search selected ;;
+		sort )
+			echo sort selected ;;
+		display_all )
+			echo display all selected ;;
+		exit )
+			echo "NOT EXITING RN !!!"
+			echo "Press ctrl + D" ;;
+		* )
+			echo Please provide a number from 1 to 5
+	esac
+done
+
