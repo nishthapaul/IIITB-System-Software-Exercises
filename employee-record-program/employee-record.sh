eb3fed58f315647d5e77cfb48478cb4ef2ddc309
Nishtha Paul <nishthapaul@Nishthas-MacBook-Air.local>
2023-08-17 18:13:24 +0530

    Add | Sort records

--- employee-record-program/employee-record.sh
+++ employee-record-program/employee-record.sh
@@ -1,25 +1,44 @@
 #!/bin/bash
 
+records_file="records.txt"
+
+function create_emp_record() {
+	read -p "Enter Employee Name ::: " e_name
+	read -p "Enter Employee Id ::: " e_id
+	read -p "Enter Employee Contact Number ::: " e_phone
+	echo $e_name $e_id $e_phone >> $records_file
+}
+
 echo "Please choose from the folowing menu to operate on employee records"
-select option in create delete search sort display_all exit
+select option in create delete search sort display_all clear_database exit
 do
 	echo option selected is ::: $option
 	case $option in
 		create )
-			echo create selected ;;
+			create_emp_record ;;
 		delete )
 			echo delete selected ;;
 		search )
 			echo search selected ;;
 		sort )
-			echo sort selected ;;
+			echo sort selected
+			sort -k 2n -k 1,1 records.txt > records.txt
+			;;
 		display_all )
-			echo display all selected ;;
+			if [ -s $records_file ]
+			then
+				cat $records_file
+			else
+				echo "No records exist !!!"
+			fi
+			;;
+		clear_database )
+			 > $records_file ;;
 		exit )
 			echo "Bye Bye !!!"
 			exit ;;
 		* )
-			echo Please provide a number from 1 to 5
+			echo Please provide a number from 1 to 6
 	esac
 done
 
