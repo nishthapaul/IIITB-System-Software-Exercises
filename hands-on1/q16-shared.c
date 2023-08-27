17d4517fdb6229649ef4501336660d6cf0e1a69e
Nishtha Paul <nishthapaul@Nishthas-MacBook-Air.local>
2023-08-27 11:42:26 +0530

    Add Q16 | Basic Shared and Exclsuive locks

---
+++ hands-on1/q16-shared.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <sys/file.h>
+#include <unistd.h>
+#include <fcntl.h>
+int main() {
+    int fd = open("q10-file.txt", O_RDWR);
+    printf("Before applying locking \n");
+    flock(fd, LOCK_SH);
+    printf("Acquiring shared lock \n");
+    getchar();
+    flock(fd, LOCK_UN);
+}
