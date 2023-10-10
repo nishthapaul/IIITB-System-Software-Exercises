c930f3435e8a17498e14a686982741e0b30af122
Nishtha Paul <nishthapaul@Nishthas-MacBook-Air.local>
2023-10-10 19:47:13 +0530

    Add | One way comm using fifo

---
+++ hands-on2/Q20/q20-client.c
@@ -0,0 +1,27 @@
+/*
+============================================================================
+Name : q20.c
+Author : Nishtha Paul
+Q. Write two programs so that both can communicate by FIFO -Use one way communication.
+Date: 10th Oct, 2023.
+
+Output:
+Input in server - My name is Nishtha
+Output in client -
+Message from server is :: My name is Nishtha
+
+Requirement:
+Create a fifo first :
+mkfifo -m 0744 myfifo
+============================================================================
+*/
+#include <unistd.h> // read()
+#include <fcntl.h> // open()
+#include <stdio.h> // printf()
+int main() {
+    char output[100];
+    int fd = open("myfifo", O_RDONLY);
+    read(fd, output, sizeof(output));
+    printf("Message from server is :: %s \n", output);
+    return 0;
+}
---
+++ hands-on2/Q20/q20-server.c
@@ -0,0 +1,19 @@
+/*
+============================================================================
+Name : q20.c
+Author : Nishtha Paul
+Q. Write two programs so that both can communicate by FIFO -Use one way communication.
+Date: 10th Oct, 2023.
+============================================================================
+*/
+#include <unistd.h> // pipe(), fork(), write(), read()
+#include <string.h> // strlen()
+#include <stdio.h> // printf(), scanf()
+#include <fcntl.h> // open(), close()
+int main() {
+    char* input;
+    int fd = open("myfifo", O_WRONLY);
+    scanf("%[^\n]", input);
+    write(fd, input, strlen(input));
+    return 0;
+}
